231fd1b9b0a23c55044ac3e8b5d987aa049a6a0e
prabuddhathecoder <prabuddhawade9@gmail.com>
2025-11-25 19:15:47 +0530

    add get user liked video
    get all like of user on video

--- src/controllers/like.controller.js
+++ src/controllers/like.controller.js
@@ -6,6 +6,7 @@ import {asyncHandler} from "../utils/asyncHandler.js"
 import {Like} from "../models/like.model.js"
 import { Video } from "../models/video.model.js"
 import { Comment } from "../models/comment.model.js"
+import { Tweet } from "../models/tweet.model.js"
 
 const toggleVideoLike = asyncHandler(async (req, res) => {
     const {videoId} = req.params
@@ -95,22 +96,79 @@ const toggleCommentLike = asyncHandler(async (req, res) => {
 const toggleTweetLike = asyncHandler(async (req, res) => {
     const {tweetId} = req.params
     //TODO: toggle like on tweet
+    if(!tweetId){
+        throw new ApiError(400,"Tweet required")
+    }
+
+    const tweet=await Tweet.findById(tweetId)
+   if(!tweet){
+        throw new ApiError(404,"tweet not found")
+    }
+
+    let liked;
+    const tweetAlredyLiked=await Like.findOne({
+        likeBy:req.user?._id,
+        tweet:tweetId
+    })
+    
+    if(tweetAlredyLiked){
+        await Like.findByIdAndDelete(tweetAlredyLiked._id)
+        liked=false
+
+    }{
+        await Like.create({
+            likeBy:req.body?._id,
+            tweet:tweetId
+        })
+        liked=true
+    }
+    // 5. Get updated like count
+    const likeCount = await Like.countDocuments({ tweet: tweetId });
+
+    // 6. Return response
+    return res.status(200).json(
+        new ApiResponse(200, {
+            liked,
+            likeCount
+        }, liked ? "tweet liked successfully" : "tweet unliked successfully")
+    );
+
 }
 )
 
 const getLikedVideos = asyncHandler(async (req, res) => {
     //TODO: get all liked videos
-    const {videoId}=req.params
-    if(!videoId){
-        throw new ApiError(400,"video not fount")
+    const userId = req.user?._id;
+
+    if (!userId) {
+        throw new ApiError(401, "User not authenticated");
     }
 
-    const getLikedVideos=await Like.findOne(
-        {
-            video:videoId,
-            likeBy:req.user?._id
-        }
-    )
+    // Find all likes by this user and populate video details
+    const likedVideos = await Like.find({ 
+        likeBy: userId // Use correct field name from your Like model
+    }).populate({
+        path: "video",
+        select: "title description thumbnail duration views owner createdAt",
+        match: { _id: { $exists: true } } // Only populate if video exists
+    })
+    .sort({ createdAt: -1 });
+  console.log('likedVideos: ',likedVideos)
+    const userLiked= likedVideos.filter(like => like.video !== null)
+   if(userLiked.length===0){
+        throw new ApiError(404,"user not liked at any video")
+    }
+
+
+    // 6. Return response
+    return res.status(200).json(
+        new ApiResponse(200, {
+            
+            user:userId,
+            likedVideos
+        },   "video liked fatch successfully")
+    );
+   
 })
 
 export {
